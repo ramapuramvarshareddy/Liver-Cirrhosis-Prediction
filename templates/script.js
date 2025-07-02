f536037417710d7bdf1b24c04031c47ea0264314
ramapuramvarshareddy <ramapuramvarshareddy@gmail.com>
2025-07-02 19:17:23 +0530

    Create script.js

---
+++ templates/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', () => {
+        alert('Submitting for prediction...');
+    });
+});
